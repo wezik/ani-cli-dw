6096ad404c79faea3ddfc3bf0beb175067441146
Dominik <wezik@users.noreply.github.com>
2024-04-15 16:38:58 +0200

    Refactor and limiter (now works)

--- downloader.go
+++ downloader.go
@@ -18,8 +18,9 @@ type DLWrapper struct {
         animeDir string
 }
 
-func downloadEpisode(dlWrapper DLWrapper, wg *sync.WaitGroup) {
+func downloadEpisode(dlWrapper DLWrapper, wg *sync.WaitGroup, semaphore chan struct{}) {
         defer wg.Done()
+        defer func() { <-semaphore }()
 
         animeEntryIdArg := fmt.Sprintf("-S %d", dlWrapper.animeEntryId)
         rangeArg := fmt.Sprintf("-e %d", dlWrapper.episode)
@@ -45,46 +46,18 @@ func downloadEpisode(dlWrapper DLWrapper, wg *sync.WaitGroup) {
         fmt.Printf("Episode %d downloaded successfully\n", dlWrapper.episode)
 }
 
+type InputWrapper struct {
+        anime string
+        animeEntryId int
+        rangeStart int
+        rangeEnd int
+        quality int
+        threadCount int
+}
+
 func main() {
-        scanner := bufio.NewScanner(os.Stdin)
-
-        fmt.Print("Enter the anime name: ")
-        scanner.Scan()
-        anime := scanner.Text()
-
-        fmt.Print("Enter the anime entry id: ")
-        scanner.Scan()
-        animeEntryId, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-                fmt.Printf("Invalid anime entry id: %v\n", err)
-                return
-        }
-
-        fmt.Print("Enter the range start: ")
-        scanner.Scan()
-        rangeStart, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-                fmt.Printf("Invalid range start: %v\n", err)
-                return
-        }
-
-        fmt.Print("Enter the range end: ")
-        scanner.Scan()
-        rangeEnd, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-                fmt.Printf("Invalid range end: %v\n", err)
-                return
-        }
-
-        fmt.Print("Enter the quality (ex. 480/720/1080): ")
-        scanner.Scan()
-        quality, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-                fmt.Printf("Invalid quality: %v\n", err)
-                return
-        }
-        
-        animeDir := filepath.Join(".", anime)
+        inputWrapper := readInput()
+        animeDir := filepath.Join(".", inputWrapper.anime, strconv.Itoa(inputWrapper.animeEntryId))
 
         if _, err := os.Stat(animeDir); os.IsNotExist(err) {
                 err := os.MkdirAll(animeDir, 0755)
@@ -95,23 +68,63 @@ func main() {
         }
 
         var wg sync.WaitGroup
+        wg.Add(inputWrapper.rangeEnd - inputWrapper.rangeStart + 1)
+        semaphore := make(chan struct{}, inputWrapper.threadCount)
 
-        wg.Add(rangeEnd - rangeStart + 1)
+        for episode := inputWrapper.rangeStart; episode <= inputWrapper.rangeEnd; episode++ {
 
-        counter := 0
+                semaphore <- struct{}{}
 
-        for i := rangeStart; i <= rangeEnd; i++ {
-                dlWrapper := DLWrapper{anime, animeEntryId, i, quality, animeDir}
-                if (counter >= 10) {
-                        wg.Wait()
-                        counter = 0 
+                dlWrapper := DLWrapper{
+                        inputWrapper.anime,
+                        inputWrapper.animeEntryId,
+                        episode,
+                        inputWrapper.quality,
+                        animeDir,
                 }
-                go downloadEpisode(dlWrapper, &wg)
-                counter++
+
+                go downloadEpisode(dlWrapper, &wg, semaphore)
         }
 
         wg.Wait()
 
         fmt.Println("All episodes downloaded")
 }
+
+func readInput() InputWrapper {
+        wrapper := InputWrapper{}
+        scanner := bufio.NewScanner(os.Stdin)
+
+        scan := func (label string, scanner *bufio.Scanner) string {
+                fmt.Print(label)
+                scanner.Scan()
+                return scanner.Text()
+        }
+
+        wrapper.anime = scan("Enter the anime name: ", scanner)
+        animeEntryIdString := scan("Enter the anime entry id: ", scanner)
+        rangeStartString := scan("Enter the range start: ", scanner)
+        rangeEndString := scan("Enter the range end: ", scanner)
+        qualityString := scan("Enter the quality (ex. 480/720/1080): ", scanner)
+        threadCountString := scan("How many simultaneous downloads do you want to hold: ", scanner)
+
+        atoiWithHandling := func(value string) int {
+                num, err := strconv.Atoi(value)
+                if err != nil {
+                        message := fmt.Sprintf("Invalid input for %s: %v", value, err)
+                        fmt.Println(message)
+                        os.Exit(1)
+                }
+                return num
+        }
+
+        wrapper.animeEntryId = atoiWithHandling(animeEntryIdString)
+        wrapper.rangeStart = atoiWithHandling(rangeStartString)
+        wrapper.rangeEnd = atoiWithHandling(rangeEndString)
+        wrapper.quality = atoiWithHandling(qualityString)
+        wrapper.threadCount = atoiWithHandling(threadCountString)
+
+        return wrapper
+}
+
 
